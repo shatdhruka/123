332975176d8a21354809909f70f3d72ea8ea5f55
shatdhruka <42232515+shatdhruka@users.noreply.github.com>
2019-03-04 14:46:28 +0530

    Create multiple 13.c

---
+++ multiple 13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    if(N%13==0)
+    {
+        printf("yes",N);
+    }
+    else
+    {
+        printf("no",N);
+    }
+return 0;
+}

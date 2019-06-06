7334b61160914d112b16492d2da2334c28a08c34
shatdhru <42232515+shatdhruka@users.noreply.github.com>
2019-06-06 15:31:52 +0530

    Create round_off.java

---
+++ round_off.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.*;
+import java.lang.Math;
+
+class round_off 
+{
+	public static void main (String[] args) 
+	{
+	    Scanner sc=new Scanner(System.in);
+	    float n=sc.nextFloat();
+		System.out.println(Math.round(n));
+	}
+}

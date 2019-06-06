094a7b70827cdccada363d46073ff160e8dd6121
shatdhru <42232515+shatdhruka@users.noreply.github.com>
2019-06-06 16:24:52 +0530

    Create Capital.java

---
+++ Capital.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.io.*;
+class Capital
+{
+	public static void main (String[] args) 
+	{
+	    Scanner sc=new Scanner(System.in);
+	    String str=sc.nextLine();
+	    String cap = str.substring(0, 1).toUpperCase() + str.substring(1);
+		System.out.println(cap);
+	}
+}

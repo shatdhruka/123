5594be80813f4f8cd337d2c539466992fcba1746
shatdhru <42232515+shatdhruka@users.noreply.github.com>
2019-06-07 09:16:35 +0530

    Create Greater_number.java

---
+++ Greater_number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.io.*;
+
+class Greater_number
+{
+	public static void main (String[] args) 
+	{
+	    Scanner sc=new Scanner(System.in);
+	    long n=sc.nextLong();
+	    long m=sc.nextLong();
+	    long a=m-n;
+		System.out.println(a);
+	}
+}

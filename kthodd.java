930c1708c96bedbef4e77c41d77cd6de6e6da060
shatdhru <42232515+shatdhruka@users.noreply.github.com>
2019-06-07 16:00:29 +0530

    Create kthodd.java

---
+++ kthodd.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Scanner;
+class GFG 
+{
+    static int kthOdd(int arr[], int n, int k) 
+    { 
+        for (int i = 0; i < n; i++) 
+        { 
+            if (arr[i] % 2 == 1) 
+                k--; 
+            if (k == 0) 
+                return arr[i]; 
+        } 
+        return -1; 
+    } 
+	public static void main (String[] args) 
+	{
+	    Scanner sc= new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int k=sc.nextInt();
+	    int arr[]=new int[n];
+	    for(int i=0;i<+n;i++)
+	    {
+	        arr[i]=sc.nextInt();
+	    }
+	    System.out.println(kthOdd(arr, n, k));
+	}
+}

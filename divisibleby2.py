ede0b1549142ad7d3b592bf063eff4fb9a41a726
shatdhru <42232515+shatdhruka@users.noreply.github.com>
2019-06-07 15:26:24 +0530

    Create divisibleby2.py

---
+++ divisibleby2.py
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+class GFG 
+{
+	public static void main (String[] args) 
+	{
+	    Scanner sc= new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int a;
+	    a=n/2;
+	    
+	    System.out.println(a);
+	}
+}
